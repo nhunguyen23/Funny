3ed671ae09c47171cd5fe301aa003d2b86eb8294
nhunguyen23 <86816073+nhunguyen23@users.noreply.github.com>
2021-07-02 11:47:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hello!',
+    introDesc: `Tớ có điều này muốn nói, nhớ trả lời thật lòng nhâ`,
+    btnIntro: 'Ok',
+    title: 'Cậu có thích mình không??',
+    desc: 'nếu không trả lời mà thoát là đồng nghĩa cậu yêu mình đấy :)) ',
+    btnYes: 'Thích nhiều lắm luôn đó <33',
+    btnNo: 'Gớm! Ai thèm thích :v',
+    question:'Nói cho tớ lí do cậu thích tớ đi <3',
+    btnReply: 'Vì cậu dễ thương lắm lun <3',
+    reply: 'Thôi ngại lắm :3',
+    mess: 'Tớ  biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h qua đón tớ đi chơi nhá :)).',
+    btnAccept: 'oke luôn!!',
+    messLink: 'http://m.me/nhu.nguyen14 //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
